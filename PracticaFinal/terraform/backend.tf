8e7f57e78aacbe45ad5e7e699afc915d39133f04
Arrate2 <claudia.arratee@gmail.com>
2025-11-19 12:12:08 +0000

    backend

--- PracticaFinal/terraform/backend.tf
+++ PracticaFinal/terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "claudia-gitops-buckt" 
-    key            = "terraform.tfstate" 
-    region         = "us-east-1" 
-    encrypt        = true 
+    bucket  = "claudia-gitops-buckt"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }

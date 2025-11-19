d23d467d13d13478e1d68ee1f7d72c6ddf40625a
Arrate2 <claudia.arratee@gmail.com>
2025-11-19 12:46:52 +0000

    c

--- PracticaFinal/terraform/main.tf
+++ PracticaFinal/terraform/main.tf
@@ -281,7 +281,7 @@ resource "aws_db_instance" "rds_postgres" {
   storage_type      = "gp2"
   engine            = "postgres"
   engine_version    = "15.2"
-  instance_class    = "db.t3.small"
+  instance_class    = "db.t3.micro"
   db_name           = var.db_name
   username          = var.db_user
   password          = var.db_pass

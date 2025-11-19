fa18b798f9a053c948b479ab9864dfb4c695c02d
Arrate2 <claudia.arratee@gmail.com>
2025-11-19 12:55:46 +0000

    c

--- PracticaFinal/terraform/main.tf
+++ PracticaFinal/terraform/main.tf
@@ -280,13 +280,13 @@ resource "aws_db_instance" "rds_postgres" {
   allocated_storage = 20
   storage_type      = "gp2"
   engine            = "postgres"
-  engine_version    = "15.2"
+  engine_version    = "15"
   instance_class    = "db.t3.micro"
   db_name           = var.db_name
   username          = var.db_user
   password          = var.db_pass
 
-  multi_az            = true
+  multi_az            = false
   skip_final_snapshot = true
 
   vpc_security_group_ids = [aws_security_group.sg_rds.id]
